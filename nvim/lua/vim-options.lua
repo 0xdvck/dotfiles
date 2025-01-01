60bed70cf1156c2bfca338298bafa802e325d9f7
0xdvck <67353104+0xdvck@users.noreply.github.com>
2025-01-02 06:40:15 +0700

    Update vim-options.lua

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -9,3 +9,10 @@ vim.keymap.set("x", "<leader>p", '"_dP')
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "dw", 'vb"_d')
+
+-- set system clipboard
+vim.keymap.set('n','y','"+y')
+vim.keymap.set('n','yy','"+yy')
+vim.keymap.set('n','Y','"+Y')
+vim.keymap.set('x','y','"+y')
+vim.keymap.set('x','Y','"+Y')
